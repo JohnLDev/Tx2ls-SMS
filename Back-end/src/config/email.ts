a37462e3e42cc3f90081b5c47b7d10c99a4702b3
johnldev <john.lenon.dev@gmail.com>
2020-11-02 14:15:17 -0300

    .env-example

--- Back-end/src/config/email.ts
+++ Back-end/src/config/email.ts
@@ -2,21 +2,12 @@ import Nodemailer from 'nodemailer'
 import { config } from 'dotenv'
 
 config()
-// const Mailer = Nodemailer.createTransport({
-//   host: 'smtp.sendgrid.net',
-//   port: 587,
-//   auth: {
-//     user: 'apikey',
-//     pass: process.env.EMAIL_API_KEY,
-//   },
-// })
-
 const Mailer = Nodemailer.createTransport({
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: 'smtp.sendgrid.net',
+  port: 587,
   auth: {
-    user: 'b089cbacf6346c',
-    pass: 'da2d2b9799cf99',
+    user: 'apikey',
+    pass: process.env.EMAIL_API_KEY,
   },
 })
 
